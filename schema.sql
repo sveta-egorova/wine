81f5229e34b6e374c440ddbee324cdfc1ae80ee3
Sveta Egorova <svetlanaegorova91@gmail.com>
2020-11-16 19:12:37 +0000

    changed schema

--- schema.sql
+++ schema.sql
@@ -1,21 +1,19 @@
 create table activity
 (
 	id int
-		constraint activity_pk
 			primary key,
 	likes_count int,
-	"comments count" int
+	comments_count int
 );
 
 create table country
 (
-	code text
-		constraint country_pk
+	code char(4)
 			primary key,
-	name text,
-	native_name text,
-	seo_name text,
-	currency text,
+	name varchar(255),
+	native_name varchar(255),
+	seo_name varchar(255),
+	currency_code char(4),
 	regions_count int,
 	users_count int,
 	wines_count int,
@@ -25,19 +23,17 @@ create table country
 create table food
 (
 	id int
-		constraint food_pk
 			primary key,
-	name text,
-	seo_name text
+	name varchar(255),
+	seo_name varchar(255)
 );
 
 create table grape
 (
 	id int
-		constraint grapes_pk
 			primary key,
-	name text,
-	seo_name text,
+	name varchar(255),
+	seo_name varchar(255),
 	has_detailed_info boolean,
 	wines_count int
 );
@@ -46,90 +42,77 @@ create table grape_country
 (
 	grape_id int
 		references grape,
-	country_code text
+	country_code char(4)
 		references country,
-	constraint grape_country_pk
-		unique (grape_id, country_code)
+	primary key (grape_id, country_code)
 );
 
 create table keyword
 (
 	id int
-		constraint keyword_pk
 			primary key,
-	word text,
-	flavor_group text
+	word varchar(255),
+	flavor_group varchar(255)
 );
 
 create table price
 (
 	id int
-		constraint price_pk
 			primary key,
 	amount float,
 	discounted_from float,
-	type text,
+	type varchar(255),
 	visibility int,
-	currency_code text,
-	bottle_type text
+	currency_code char(4),
+	bottle_type varchar(255)
 );
 
 create table region
 (
 	id int
-		constraint region_pk
 			primary key,
-	name text,
-	name_en text,
-	seo_name text,
-	country_code text
+	name varchar(255),
+	name_en varchar(255),
+	seo_name varchar(255),
+	country_code char(4)
 		references country
-);
-
-create table test_table
-(
-	code text,
-	name text
 );
 
 create table toplist
 (
 	id int
-		constraint toplist_pk
 			primary key,
-	location text,
-	name text,
-	seo_name text,
+	location varchar(255),
+	name varchar(255),
+	seo_name varchar(255),
 	type int,
 	year int
 );
 
 create table type
 (
-	type_id int
-		constraint wine_type_pk
+	id int
 			primary key,
-	type_name text
+	name varchar(255)
 );
 
 create table style
 (
 	id int
-		constraint style_pk
 			primary key,
-	seo_name text,
-	regional_name text,
-	varietal_name text,
-	name text,
-	description text,
-	blurb text,
+	seo_name varchar(255),
+	regional_name varchar(255),
+	varietal_name varchar(255),
+	name varchar(255),
+	description varchar(255),
+	blurb varchar(255),
 	body int,
-	body_description text,
+	body_description varchar(255),
 	acidity int,
-	acidity_description text,
-	country text
+	acidity_description varchar(255),
+	country_code char(4)
 		references country,
-	wine_type_id int
+	type_id int
 		references type
 );
 
@@ -137,7 +120,9 @@ create table facts
 (
 	style_id int
 		references style,
-	fact text
+	fact varchar(255),
+    primary key (style_id, fact)
+
 );
 
 create table style_food
@@ -146,8 +131,7 @@ create table style_food
 		references style,
 	food_id int
 		references food,
-	constraint style_food_pk
-		unique (style_id, food_id)
+	primary key (style_id, food_id)
 );
 
 create table style_grape
@@ -156,19 +140,17 @@ create table style_grape
 		references style,
 	grape_id int
 		references grape,
-	constraint style_grape_pk
-		unique (style_id, grape_id)
+	primary key (style_id, grape_id)
 );
 
 create table user
 (
 	id int
-		constraint user_pk
 			primary key,
-	seo_name text,
-	alias text,
+	seo_name varchar(255),
+	alias varchar(255),
 	is_featured boolean,
-	visibility text,
+	visibility varchar(255),
 	followers_count int,
 	followings_count int,
 	ratings_count int,
@@ -179,18 +161,17 @@ create table user
 create table review
 (
 	id int
-		constraint review_pk
 			primary key,
 	rating float,
 	note text,
-	language text,
-	"created at" text,
+	language varchar(255),
+	created_at datetime,
 	aggregated boolean,
 	user_id int
 		references user,
 	activity_id int
 		references activity,
-	tagged_note text
+	tagged_note varchar(255)
 );
 
 create table review_keyword
@@ -199,27 +180,24 @@ create table review_keyword
 		references review,
 	keyword_id int
 		references keyword,
-	constraint review_keyword_pk
-		unique (review_id, keyword_id)
+	primary key (review_id, keyword_id)
 );
 
 create table winery
 (
 	id int
-		constraint winery_pk
 			primary key,
-	name text,
-	seo_name text,
+	name varchar(255),
+	seo_name varchar(255),
 	status int
 );
 
 create table wine
 (
 	id int
-		constraint wine_pk
 			primary key,
-	name text,
-	seo_name text,
+	name varchar(255),
+	seo_name varchar(255),
 	type_id int
 		references type,
 	vintage_type int,
@@ -237,7 +215,7 @@ create table wine
 	calculated_structure_count int,
 	style_id int
 		references style,
-	status text,
+	status varchar(255),
 	ratings_count int,
 	ratings_average float,
 	labels_count int,
@@ -248,15 +226,14 @@ create table wine
 create table vintage
 (
 	id int
-		constraint vintage_pk
 			primary key,
-	seo_name text,
-	name text,
+	seo_name varchar(255),
+	name varchar(255),
 	wine_id int
 		references wine,
 	year int,
 	has_valid_ratings boolean,
-	status text,
+	status varchar(255),
 	ratings_count int,
 	ratings_average float,
 	labels_count int,
@@ -270,8 +247,7 @@ create table vintage_review
 		references vintage,
 	review_id int
 		references review,
-	constraint vintage_review_pk
-		unique (vintage_id, review_id)
+	primary key (vintage_id, review_id)
 );
 
 create table vintage_toplist
@@ -280,24 +256,21 @@ create table vintage_toplist
 		references vintage,
 	toplist_id int
 		references toplist,
-	rank int,
+	`rank` int,
 	previous_rank int,
-	description text,
-	constraint vintage_toplist_pk
-		unique (vintage_id, toplist_id)
+	description varchar(255),
+	primary key (vintage_id, toplist_id)
 );
 
 create table wine_flavor_group
 (
 	wine_id int
 		references wine,
-	flavor_group_name text
-		constraint wine_flavor_group_keyword_flavor_group_fk
+	flavor_group_name varchar(255)
 			references keyword (flavor_group),
 	count int,
 	score int,
-	constraint wine_flavor_group_pk
-		unique (wine_id, flavor_group_name)
+	primary key (wine_id, flavor_group_name)
 );
 
 create table wine_keyword
@@ -307,7 +280,5 @@ create table wine_keyword
 	keyword_id int
 		references keyword,
 	count int,
-	constraint wine_keyword_pk
-		unique (wine_id, keyword_id)
+	primary key (wine_id, keyword_id)
 );
-
