f0ce57a6a9cc131d68cb9f0000acc8bdf4d7a3dd
Sveta Egorova <svetlanaegorova91@gmail.com>
2020-12-03 16:21:03 +0000

    Refactoring

---
+++ crawlers.py
@@ -0,0 +1,189 @@
+from typing import List, Dict
+import requests
+from ratelimiter import RateLimiter
+import json
+import time
+import math
+import pickle
+import pandas as pd
+
+
+class Crawler:
+
+    def __init__(self, session):
+        self.session = session
+
+    @RateLimiter(max_calls=1, period=1)
+    def _call_to_api(self, s, page):
+        """
+        Function extracting total number of records and JSON list from vivino API explore endpoint with rate limiting
+        """
+        response = s.get(page)
+        json_str = response.content
+        try:
+            json_obj = json.loads(json_str)
+            return json_obj
+        except:
+            print(response.content)
+
+    def _parse_vintages(self, s, page_num: int, price_min: int, price_max: int):
+        """
+        Function that extracts records from a single page
+        """
+        page_template = 'https://www.vivino.com/api/explore/explore?country_code=GB&currency_code=GBP' \
+                        '&grape_filter=varietal&min_rating=1&order_by=ratings_average&order=desc&page={}' \
+                        '&per_page=100&price_range_min={}&price_range_max={}'
+        page = page_template.format(page_num, price_min, price_max)
+        json_obj = self._call_to_api(s, page)
+        return json_obj['explore_vintage']['matches']
+
+    def _parse_vintage_num(self, s, page_num: int, price_min: int, price_max: int):
+        """
+        Function that extracts total number of matches
+        """
+        page_template = 'https://www.vivino.com/api/explore/explore?country_code=GB&currency_code=GBP' \
+                        '&grape_filter=varietal&min_rating=1&order_by=ratings_average&order=desc&page={}' \
+                        '&per_page=100&price_range_min={}&price_range_max={}'
+        page = page_template.format(page_num, price_min, price_max)
+        json_obj = self._call_to_api(s, page)
+        return json_obj['explore_vintage']['records_matched']
+
+    def _parse_reviews(self, s, wine_id: int, year: str, page_num: int):
+        """
+        Function that returns reviews extracted for a particular wine ID and year, and a particular page
+        """
+        page_template = 'https://www.vivino.com/api/wines/{}/latest_reviews?year={}&per_page=50&page={}'
+        page = page_template.format(wine_id, year, page_num)
+        json_obj = self._call_to_api(s, page)
+        return json_obj['reviews']
+
+    def download_reviews(self, df: pd.DataFrame, country: str, year, backup=True, verbose=True,
+                         backup_directory="backup_data/reviews/") -> List[Dict]:
+        """
+        Function that returns all reviews extracted for a particular wine ID and year, and appends them to a given list.
+        """
+        s = requests.Session()
+        s.headers.update({
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.193 Safari/537.36',
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        })
+
+        data = df[(df['country'] == country) & (df['year'] == year)].sort_values('rating_count', ascending=False)
+        reviews = []
+        timepoint_0 = time.time()
+
+        for index, row in data.iterrows():
+            wine_id = row['wine_id']
+            num = row['rating_count']
+
+            max_pages = math.ceil(num / 50)
+
+            if verbose:
+                print(
+                    f"The program will send up to {max_pages} requests to reviews API to extract up to {num} reviews for wine {wine_id}")
+
+            for it in range(1, max_pages + 1):
+                reviews_batch = self._parse_reviews(s, wine_id, year, it)
+                if len(reviews_batch) == 0:
+                    break
+                else:
+                    reviews += reviews_batch
+
+            if verbose and index % 100 == 0:
+                print(
+                    f"Record no. {index} with id {wine_id} finished. Currently stores {len(set([review['id'] for review in reviews]))} records")
+
+        if verbose:
+            print(
+                f"""Program uploaded {len(set([review['id'] for review in reviews]))} reviews for {country} for the year {year}. 
+        It took app. {round((time.time() - timepoint_0) / 60, 2)} minutes to run""")
+
+        # if backup:
+        #     with open(f"{backup_directory}{country}_{year}", 'wb') as f:
+        #         pickle.dump(reviews, f)
+        s.close()
+
+        return reviews
+
+    def download_all_wines(self, price_min=0, price_max=400, with_prices=True, inter_backup=True, final_backup=True,
+                           verbose=True):
+        """
+        Function that iterates over small price ranges to extract all the data within a given range between min price and max price.
+        If necessary, the function may store intermediate backups and/or a final backup inside pickle files.
+        """
+        s = requests.Session()
+        s.headers.update({
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.193 Safari/537.36',
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        })
+
+        if verbose:
+
+            records_total = self._parse_vintage_num(s, 1, price_min, price_max)
+
+            if price_min == 0 and price_max == 400 and with_prices is True:
+                # here, we remove all records with no prices. Those appear only when range (0, 400) is called
+                records_total -= self._parse_vintage_num(s, 1, 0, 0)
+
+            print(
+                f'''The program recognized app.{records_total} unique records with a price from {price_min} to {price_max} 
+                and will run for app.{math.ceil(records_total / 100)} iterations in total''')
+
+        full_wine_list = []
+        timepoint_start = time.time()
+
+        # define iteration variables
+        timepoint_iter = timepoint_start
+        cur_batch = []
+
+        if price_min == 0 and price_max == 400 and with_prices is not True:  # remove wines with no prices
+            # here, we remove all records with no prices. Those appear only when range (0, 400) is called
+            records_total -= self._parse_vintage_num(1, 0, 0)
+
+        # load each price
+        for i in range(price_min, price_max):
+            if i == 0 and with_prices is not True:  # special case to cover wines with no prices
+                cur_price_records_num = self._parse_vintage_num(s, 1, i,
+                                                                i)  # TODO consider the case when iterations exceed 80
+            else:
+                cur_price_records_num = self._parse_vintage_num(s, 1, i, i + 1)
+            # to make sure the necessary number of iterations to catch all of the records with a given price:
+            iterations_required = math.ceil(cur_price_records_num / 100)
+            # load each page of a given price
+            for it in range(1, iterations_required + 1):
+                if i == 0 and with_prices is not True:  # special case to cover wines with no prices
+                    cur_batch += self._parse_vintages(s, 1, i, i)
+                else:
+                    cur_batch += self._parse_vintages(it, i, i + 1)
+                # for every 10th price write intermediate backup, send the results to the main list and update the batch
+                if i % 10 == 0 and it == iterations_required:
+                    if inter_backup:
+                        # piece of code necessary to write intermediate backups in the process
+                        with open(f"backup_data/match_list_{i - 1}", 'wb') as f:
+                            pickle.dump(cur_batch, f)
+                    # append the batch results to the main list
+                    full_wine_list.extend(cur_batch)
+                    cur_batch = []
+
+                    if verbose:
+                        time_batch = time.time() - timepoint_iter
+                        print(
+                            f'''Bunch of records with price below {i + 1} uploaded and took {round(time_batch / 60, 2)} minutes. 
+                            Next, running prices from {i + 1}...''')
+                        timepoint_iter = time.time()
+
+            full_wine_list.extend(cur_batch)
+
+        if final_backup:
+            with open(f"backup_data/full_match_list", 'wb') as f:
+                pickle.dump(full_wine_list, f)
+
+        if verbose:
+            total_time = time.time() - timepoint_start
+            print(
+                f'''Program finished and successfully uploaded {len(full_wine_list)} wine records with prices(need to check duplicates).
+                The program took app. {round(total_time / 60, 2)} minutes to run''')
+
+        return full_wine_list
--- test.py
+++ test.py
@@ -1,40 +1,123 @@
+import os
+import sys
 
+from crawlers import *
+import requests
+import pandas as pd
+
+
+def remove_wine_duplicates(json_data):
+    distinct_dict = {entry['vintage']['id']: entry for entry in json_data}
+    data_distinct = distinct_dict.values()
+    return list(data_distinct)
+
+
+def remove_review_duplicates(json_data):
+    distinct_dict = {entry['id']: entry for entry in json_data}
+    data_distinct = distinct_dict.values()
+    return list(data_distinct)
+
+
+def read_and_preprocess_wines(path, country, year):
+    with open(path, 'rb') as f:
+        recovered_data = pickle.load(f)
+    full_df = pd.DataFrame(remove_wine_duplicates(recovered_data))
+    full_df = pd.json_normalize(full_df['vintage'])
+    selected_columns = full_df[full_df['has_valid_ratings'] == True]\
+        [['id', 'year', 'statistics.ratings_count', 'wine.id', 'wine.region.country.name']]
+    selected_columns.columns = ['id', 'year', 'rating_count', 'wine_id', 'country']
+    data = selected_columns[(selected_columns['country'] == country) &
+                            (selected_columns['year'] == year)]\
+        .sort_values('rating_count', ascending=False)
+    return data
+
+
+def read_and_preprocess_reviews(country, year, backup_directory="backup_data/reviews/"):
+    with open(f"{backup_directory}{country}_{year}", 'rb') as f:
+        recovered_data = pickle.load(f)
+    full_df = pd.json_normalize(remove_review_duplicates(recovered_data))
+    selected_columns = full_df[(full_df['vintage.wine.region.country.name'] == country)
+                               & (full_df['vintage.year'] == year)]
+    # selected_columns.columns = ['id', 'year', 'rating_count', 'wine_id', 'country']
+    return selected_columns
+
+# def filter_by_country_year(dataframe, country: str, year: str):
+#     data = dataframe[(dataframe['country'] == country) & (dataframe['year'] == year)]
+#     return data.sort_values('rating_count', ascending=False)
+
+
+# data_Italy_2014 = filter_df[(filter_df['country'] == 'Italy') & (filter_df['year'] == 2014)].sort_values('reviews_count', ascending=False)
+#
+#
 
 if __name__ == "__main__":
-    import pickle
-    import pandas as pd
+    """
+    Usage: python test.py France 2019
+    """
 
-    with open(f"backup_data/full_match_list", 'rb') as f:
-        recovered_data = pickle.load(f)
+    country = sys.argv[1]
+    year = int(sys.argv[2])
+
+    crawler = Crawler()
+
+    full_list_file = "backup_data/full_match_list"
+    if os.path.isfile(full_list_file):
+        wines = read_and_preprocess_wines(full_list_file, country, year)
+    else:
+        wines = crawler.download_all_wines(105, 107, inter_backup=False, final_backup=False, verbose=True)
+        save_wines(full_list_file, wines)
+        wines = preprocess_wines(wines, country, year)
+
+    reviews = crawler.download_reviews(wines)
+    s.close()
+    save_reviews("backup_directory/bla/", country, year, reviews)
+
+    # reviews = read_and_preprocess_reviews(country, year)  # TODO read_and_preprocess_reviews must include 2 calls: read and preprocess
+    reviews = deduplicate_and_filter_reviews(reviews, year)
+    print(f"After processing, the data on {country} and {year} includes {reviews['id'].nunique()} unique reviews")
 
 
-    def remove_wine_duplicates(json_data):
-        distinct_dict = {entry['vintage']['id']: entry for entry in json_data}
-        recovered_data_distinct = distinct_dict.values()
-        return list(recovered_data_distinct)
+
+    # print(reviews.groupby('vintage.year').count()['id'])
+    # print(f"Hey, I extracted a dataframe of shape {reviews.shape}")
+    # print(reviews.iloc[:5,:5])
+
+    #
+    # wine_crawler = Crawler(s)
+    # results = wine_crawler.load_all_wines(105, 107, inter_backup=False, final_backup=False, verbose=True)
+
+    # data_filtered = filter_by_country_year(data, 'France', 2013)
+    # print(f"Hey, I extracted a dataframe of shape {data_filtered.shape}")
+    # print(data_filtered.head())
 
 
-    recovered_data_distinct = remove_wine_duplicates(recovered_data)
 
-    full_df = pd.DataFrame(recovered_data_distinct)
-    full_df_normalized = pd.json_normalize(full_df['vintage'])
+#     with open(f"backup_data/full_match_list", 'rb') as f:
+#         recovered_data = pickle.load(f)
+#
+#
+#     def remove_wine_duplicates(json_data):
+#         distinct_dict = {entry['vintage']['id']: entry for entry in json_data}
+#         recovered_data_distinct = distinct_dict.values()
+#         return list(recovered_data_distinct)
+#
+#
+#     recovered_data_distinct = remove_wine_duplicates(recovered_data)
+#
+#     full_df = pd.DataFrame(recovered_data_distinct)
+#     full_df_normalized = pd.json_normalize(full_df['vintage'])
+#
+#     review_filtering_df = full_df_normalized[
+#         ['id', 'year', 'has_valid_ratings', 'statistics.ratings_count', 'wine.id', 'wine.region.country.name']]
+#
+#     filter_df = review_filtering_df.loc[
+#         review_filtering_df['has_valid_ratings'] == True, ['id', 'year', 'statistics.ratings_count',
+#                                                            'wine.id', 'wine.region.country.name']]
+#
+#     filter_df.columns = ['id', 'year', 'reviews_count', 'wine_id', 'country']
+#
+#     data_Italy_2015 = filter_df[(filter_df['country'] == 'Italy') & (filter_df['year'] == 2015)].sort_values(
+#         'reviews_count', ascending=False)
 
-    review_filtering_df = full_df_normalized[
-        ['id', 'year', 'has_valid_ratings', 'statistics.ratings_count', 'wine.id', 'wine.region.country.name']]
-
-    filter_df = review_filtering_df.loc[
-        review_filtering_df['has_valid_ratings'] == True, ['id', 'year', 'statistics.ratings_count',
-                                                           'wine.id', 'wine.region.country.name']]
-
-    filter_df.columns = ['id', 'year', 'reviews_count', 'wine_id', 'country']
-
-    data_Italy_2015 = filter_df[(filter_df['country'] == 'Italy') & (filter_df['year'] == 2015)].sort_values(
-        'reviews_count', ascending=False)
-
-#%%
-
-data_Italy_2015.head(20)
-
-#%%
-
+# data_Italy_2015.head(20)
 
